043dd342a1dbd0214c53af9023c8cf93dde2f152
santoshj <santoshj@vmware.com>
2016-02-14 12:03:12 -0800

    Added Calculator.java and updated main

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,11 +2,13 @@ import java.io.Writer;
 import java.io.FileWriter;
 public class JavaHelloWorld{
         public static void main(String[] args){
-                System.out.println("Java Hello World - new checkin");
-                System.out.println("Hello again - with new checkin");
+                System.out.println("Java Hello World - new checkin,eclipse");
+                System.out.println("Hello again - with new checkin,eclipse");
+                Calculator calc = new Calculator();
+                System.out.println("The value after adding two numbers 2,3 is " + calc.add(2, 3));
                 Writer w = null;
                 try {                   
-                        String s = "output.txt";
+                        String s = "output.txt,eclipse";
                         System.out.println(s);
                         w = new FileWriter(s);
                         w.write("hello");
